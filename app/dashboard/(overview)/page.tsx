10d309f37ddc6ecf400cf7e44566520df23907d8
SerdarMenekli <116300043+SerdarMenekli@users.noreply.github.com>
2024-10-11 10:59:23 +0300

    eslint fixes

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,11 +2,11 @@
 //     return <p>Dashboard Page</p>
 // }
 
-import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper/*, { Card }*/ from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+// import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
